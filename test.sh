837d868728b6c0e012e615aa659a301ba02b1dd2
Kelvin Loh <kel85uk@gmail.com>
2014-08-01 20:51:16 +0200

    V0.77b

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 SIGMA1=(0 1 2 3 4 5 6 7 8 9)
-SIGMA2=(2 4 6 8) #Decimals
-LAMBDA=(0.1 0.15 0.2 0.25 0.3 0.35 0.4) #Lambda
+SIGMA2=(1 2 4 6 8) #Decimals
+LAMBDA=( 0.1 0.15 0.2 0.25 0.3 0.35 0.4 ) #Lambda
 
 # Of the form -sigma ${SIGMA1}.${SIGMA2}
 
@@ -10,7 +10,7 @@ for SIG1 in ${SIGMA1[@]}
 do
 for SIG2 in ${SIGMA2[@]}
 do
-for LAM in ${LAMBDA[@]}
+for LAM in "${LAMBDA[@]}"
 do
         SIG="${SIG1}.${SIG2}"
         # set the job name
@@ -21,7 +21,6 @@ do
         # This requests 1 node for 1 hour. Runs a program called "my_program" with an argument.
 
         PBS="#!/bin/bash\n\
-        #PBS -N ${NAME}\n\
         #PBS -l nodes=1:ppn=32\n\
         #PBS -l walltime=08:00:00\n\
         #PBS -q normal\n\
@@ -30,17 +29,16 @@ do
         . /etc/profile.d/modules.sh\n\
         module load gcc openmpi cuda boost cmake\n\
         cd /home/stud/ug30owag/Documents/GMRF_PETSc\n\
-        mpirun -np 32 --report-bindings --bind-to-core ./Test5 -mat_type seqaij -vec_type seq -Nsamples 2000 -m 500 -n 500 -pc_type hypre -ksp_type fgmres -dim 2 -alpha 2 -lamb ${LAM} -sigma ${SIG} -TOL 1e-10"
+        mpirun -np 32 --report-bindings --bind-to-core ./Test5 -ppc 1 -mat_type seqaij -vec_type seq -Nsamples 2000 -m 500 -n 500 -pc_type hypre -pc_hypre_boomeramg_max_iter 2 -ksp_type richardson -ksp_max_it 500 -dim 2 -alpha 2 -lamb ${LAM} -sigma ${SIG} -TOL 1e-10"
         # Note that $PBS_O_WORKDIR is escaped ("\"). We don't want bash to evaluate this variable right now. Instead it will be evaluated when the command runs on the node.
 
         # Echo the string PBS to the function qsub, which submits it as a cluster job for you
         # A small delay is included to avoid overloading the submission process
 
-        echo -e ${PBS} #| qsub 
-        sleep 0.5
+        echo -e ${PBS} | qsub
+        sleep 1.0
         echo "done."
 
 done
 done
 done
-
