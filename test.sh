9ce62bd3cd0dbd47d2d480b3898175d88da34c2e
Kelvin Loh <kel85uk@gmail.com>
2014-08-05 11:20:33 +0200

    V0.77b

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 SIGMA1=(0 1 2 3 4 5 6 7 8 9)
 SIGMA2=(1 2 4 6 8) #Decimals
 LAMBDA=( 0.1 0.15 0.2 0.25 0.3 0.35 0.4 ) #Lambda
-PCG=( hypre icc ilu )
-SOLVER=( fgmres bcgs )
+PCG=( hypre icc ilu sor )
+SOLVER=( fgmres bcgs symmlq cgne )
 
 # Of the form -sigma ${SIGMA1}.${SIGMA2}
 
@@ -20,7 +20,7 @@ do
 for PCG1 in ${PCG[@]}
 do
         # set the job name
-        NAME="Test5_${PCG1}_${SOLVER1}_32SLMC1_500x500_2000s"
+        NAME="Test5_${PCG1}_${SOLVER1}_32SLMC1_500x500_150s"
         echo "Submitting: ${NAME}"
 
         # Build a string called PBS which contains the instructions for your run
@@ -35,16 +35,17 @@ do
         . /etc/profile.d/modules.sh\n\
         module load gcc openmpi cuda boost cmake\n\
         cd /home/stud/ug30owag/Documents/GMRF_PETSc\n\
-        mpirun -np 32 --report-bindings --bind-to-core ./Test5 -ppc 1 -mat_type seqaij -vec_type seq -Nsamples 2000 -m 500 -n 500 -pc_type ${PCG1} -ksp_type ${SOLVER1} -dim 2 -alpha 2 -lamb 0.1 -sigma 0.3 -TOL 1e-10"
+        mpirun -np 32 --report-bindings --bind-to-core ./Test5 -ppc 1 -mat_type seqaij -vec_type seq -Nsamples 150 -m 500 -n 500 -pc_type ${PCG1} -pc_factor_levels 0 -pc_sor_its 200 -pc_sor_omega 1.8 -ksp_type ${SOLVER1} -dim 2 -alpha 2 -lamb 0.1 -sigma 0.3 -TOL 1e-10"
         # Note that $PBS_O_WORKDIR is escaped ("\"). We don't want bash to evaluate this variable right now. Instead it will be evaluated when the command runs on the node.
 #mpirun -np 32 --report-bindings --bind-to-core ./Test5 -ppc 1 -mat_type seqaij -vec_type seq -Nsamples 2000 -m 500 -n 500 -pc_type hypre -pc_hypre_boomeramg_max_iter 2 -ksp_type richardson -ksp_max_it 500 -dim 2 -alpha 2 -lamb ${LAM} -sigma ${SIG} -TOL 1e-10
         # Echo the string PBS to the function qsub, which submits it as a cluster job for you
         # A small delay is included to avoid overloading the submission process
 
-#        echo -e ${PBS} | qsub
+        echo -e ${PBS} | qsub
         sleep 1.0
         echo "done."
 
 done
 done
 #done
+
